00fd23f4a633b4e8114eb0d670affcc90b333ca4
Mykola <nicholaskaradzha@gmail.com>
2022-01-27 14:30:59 +0700

    new components added

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export function Header() {
+    return <header className="header">
+        <img className="logo"
+             src="https://cdn.worldvectorlogo.com/logos/svg.svg"
+             alt="logo"/>
+    </header>;
+}
---
+++ src/components/Profile/Profile.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export function Profile() {
+    return <main className="main">
+        <div>
+            <img className="wallpaper" src="https://images4.alphacoders.com/602/602004.jpg"
+                 alt="wallpaper-lake-mountains"/>
+        </div>
+        <div>Info about me</div>
+        <div> My posts
+            <div>New post</div>
+        </div>
+    </main>;
+}
